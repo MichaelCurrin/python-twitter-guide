26248d2e508a37896516ea39aa8dabdedec3fe35
Michael Currin <michael.currin@gmail.com>
2020-05-23 22:10:00 +0200

    feat: Disable docsify and search tags

--- make_paths_static.sh
+++ make_paths_static.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 # Make paths static
+#
+# See notes in deploy.md doc.
 set -e
 
-BEFORE='#\/'
-AFTER='\/'
-PATTERN="s/${BEFORE}/${AFTER}/g; s/?id=/#/g"
+OLD_PATH='#/'
+NEW_PATH='/'
+
+OLD_ID='?id='
+NEW_ID='#'
+
+DOCSIFY_TAG='<script src="//unpkg.com/docsify/lib/docsify.min.js"></script>'
+SEARCH_TAG='<script src="//cdn.jsdelivr.net/npm/docsify/lib/plugins/search.min.js"></script>'
+
+REPLACE="s|${OLD_PATH}|${NEW_PATH}|g
+s|${OLD_ID}|${NEW_ID}|g
+s|${DOCSIFY_TAG}|<!-- ${DOCSIFY_TAG} -->|g
+s|${SEARCH_TAG}|<!-- ${SEARCH_TAG} -->|g"
 
 # Add blank value as backup parameter if macOS otherwise leave out string completely.
 # This assumes the gnu sed is not installed on macOS. Substituting didn't work so just to it
@@ -12,8 +24,8 @@ PATTERN="s/${BEFORE}/${AFTER}/g; s/?id=/#/g"
 
 if [[ $OSTYPE == "darwin"* ]]; then
   echo 'Mac'
-  sed -i '' "$PATTERN" $(find build -type f)
+  sed -i '' "$REPLACE" $(find build -type f)
 else
   echo 'Not a Mac'
-  sed -i "$PATTERN" $(find build -type f)
+  sed -i "$REPLACE" $(find build -type f)
 fi
